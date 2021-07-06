9d17a01c4c4b2cec55ad3dcc34a374b0ac64059e
janayAl <jsallen298705@gmail.com>
2021-07-06 15:02:05 -0500

    install packages

---
+++ index.js
@@ -0,0 +1,54 @@
+// TODO: Include packages needed for this application
+
+const inquirer = require('inquirer');
+const fs = require('fs');
+const util = require('util');
+const generateHTML = (ANSWERS) =>
+
+
+
+
+// TODO: Create an array of questions for user input
+const questions = [];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
+
+
+// const inquirer = require('inquirer');
+// const fs = require('fs');
+// const generateHTML = (ANSWERS) =>
+
+// inquirer
+//   .prompt([
+//     {
+//       type: 'input',
+//       message: 'GitHub username?',
+//       name: 'name',
+//     },
+//     {
+//       type: 'input',
+//       message: 'What is your email address?',
+//       name: 'input',
+//     },
+//     {
+//       type: 'input',
+//       message: 'What is your project name?',
+//       name: 'input',
+//     },
+//   ])
+//   .then((answers) => {
+//     const htmlPageContent = generateHTML(answers);
+
+
+      
+//         fs.writeFile('index.html', htmlPageContent, (err) => 
+// err ? console.log(err): console.log('Successfully created index.html!')       
+//  );
+//   });

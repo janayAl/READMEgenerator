3273758e99f773ec1bd26dac32f96b1a723d5367
janayAl <jsallen298705@gmail.com>
2021-07-08 12:32:33 -0500

    init function

--- index.js
+++ index.js
@@ -8,79 +8,86 @@ const util = require('util');
 
 
 // TODO: Create an array of questions for user input
-const questions = 
-[
-    'What is your GitHub username?',
-    'What is your email address?', 
-    'Please write a brief description of your project',
-    'What kind of license should your project have?' ,
-    'What command should be run to install dependencies?' ,
-    'What command should be run to run tests?',
-    'What does the user need to know about using the repo?',
-    'What does the user need to know about contributing to the repo?'
+const questions =
+    [
+        'What is your GitHub username?',
+        'What is your email address?',
+        'Please write a brief description of your project',
+        'What kind of license should your project have?',
+        'What command should be run to install dependencies?',
+        'What command should be run to run tests?',
+        'What does the user need to know about using the repo?',
+        'What does the user need to know about contributing to the repo?'
     ];
 const generateHTML = (ANSWERS) => "";
 
-    // TODO: Create a function to write README file
-    
-    function writeToFile(fileName, data) { }
+// TODO: Create a function to write README file
 
-    // TODO: Create a function to initialize app
-    function init() { }
+function writeToFile(fileName, data) { }
 
-    // Function call to initialize app
-    init();
+// TODO: Create a function to initialize app
+function init() { }
+// inquirer.prompt(questions)
+//     .then((inquirerResponse, data) => {
+//         console.log("Creating a ReadMe file");
+//         fs.writeFileSync("ReadMe.md", inquirerResponse, data);
+//     })
+//     .catch((err) => {
+//         console.log(err);
+//     })
 
-// inquirer
-//   .prompt([
-//     {
-//       type: 'input',
-//       message: 'What is your GitHub username?',
-//       name: 'name',
-//     },
-//     {
-//       type: 'input',
-//       message: 'What is your email address?',
-//       name: 'input',
-//     },
-//     {
-//       type: 'input',
-//       message: 'What is your project name?',
-//       name: 'input',
-//     },
-//     {
-//       type: 'input',
-//       message: 'What kind of license should your project have?',
-//       name: 'input',
-//     },
-//     {
-    //       type: 'input',
-    //       message: 'What command should be run to install dependencies?',
-    //       name: 'input',
-    //     },
-    //{
-//       type: 'input',
-//       message: 'What command should be run to run tests? ',
-//       name: 'input',
-//     },
-//      {
-//       type: 'input',
-//       message: 'What does the user need to know about using the repo?',
-//       name: 'input',
-//     },
-//      {
-//       type: 'input',
-//       message: 'What does the user need to know about contributing to the repo?',
-//       name: 'input',
-//     },
-//   ])
-//   .then((answers) => {
-//     const htmlPageContent = generateHTML(answers); })
+// Function call to initialize app
+init();
 
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            message: 'What is your GitHub username?',
+            name: 'name',
+        },
+        {
+            type: 'input',
+            message: 'What is your email address?',
+            name: 'input',
+        },
+        {
+            type: 'input',
+            message: 'What is your project name?',
+            name: 'input',
+        },
+        {
+            type: 'checkbox',
+            message: 'What kind of license should your project have?',
+            name: 'stack',
+            choices: ['MIT', 'BSD', 'GPL']
+        },
+        {
+            type: 'input',
+            message: 'What command should be run to install dependencies?',
+            name: 'input',
+        },
+        {
+            type: 'input',
+            message: 'What command should be run to run tests? ',
+            name: 'input',
+        },
+        {
+            type: 'input',
+            message: 'What does the user need to know about using the repo?',
+            name: 'input',
+        },
+        {
+            type: 'input',
+            message: 'What does the user need to know about contributing to the repo?',
+            name: 'input',
+        },
+    ])
+    .then((answers) => {
+        const htmlPageContent = generateHTML(answers);
+    })
 
-
-
-//         fs.writeFile('index.html', htmlPageContent, (err) => 
-// err ? console.log(err): console.log('Successfully created index.html!')       
-//  );
-//   });
+fs.writeFile('index.html', htmlPageContent, (err) =>
+    err ? console.log(err) : console.log('Successfully created index.html!')
+);
+  });

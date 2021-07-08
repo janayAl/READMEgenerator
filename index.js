b1f94dd3736a85c6c0dafa62ca3a6992cb74d6bf
janayAl <jsallen298705@gmail.com>
2021-07-08 11:42:32 -0500

    add quetions to the const array questions

--- index.js
+++ index.js
@@ -3,33 +3,39 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 const util = require('util');
-const generateHTML = (ANSWERS) =>
 
 
 
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = 
+[
+    'What is your GitHub username?',
+    'What is your email address?', 
+    'Please write a brief description of your project',
+    'What kind of license should your project have?' ,
+    'What command should be run to install dependencies?' ,
+    'What command should be run to run tests?',
+    'What does the user need to know about using the repo?',
+    'What does the user need to know about contributing to the repo?'
+    ];
+const generateHTML = (ANSWERS) => "";
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+    // TODO: Create a function to write README file
+    
+    function writeToFile(fileName, data) { }
 
-// TODO: Create a function to initialize app
-function init() {}
+    // TODO: Create a function to initialize app
+    function init() { }
 
-// Function call to initialize app
-init();
-
-
-// const inquirer = require('inquirer');
-// const fs = require('fs');
-// const generateHTML = (ANSWERS) =>
+    // Function call to initialize app
+    init();
 
 // inquirer
 //   .prompt([
 //     {
 //       type: 'input',
-//       message: 'GitHub username?',
+//       message: 'What is your GitHub username?',
 //       name: 'name',
 //     },
 //     {
@@ -42,12 +48,38 @@ init();
 //       message: 'What is your project name?',
 //       name: 'input',
 //     },
+//     {
+//       type: 'input',
+//       message: 'What kind of license should your project have?',
+//       name: 'input',
+//     },
+//     {
+    //       type: 'input',
+    //       message: 'What command should be run to install dependencies?',
+    //       name: 'input',
+    //     },
+    //{
+//       type: 'input',
+//       message: 'What command should be run to run tests? ',
+//       name: 'input',
+//     },
+//      {
+//       type: 'input',
+//       message: 'What does the user need to know about using the repo?',
+//       name: 'input',
+//     },
+//      {
+//       type: 'input',
+//       message: 'What does the user need to know about contributing to the repo?',
+//       name: 'input',
+//     },
 //   ])
 //   .then((answers) => {
-//     const htmlPageContent = generateHTML(answers);
+//     const htmlPageContent = generateHTML(answers); })
 
 
-      
+
+
 //         fs.writeFile('index.html', htmlPageContent, (err) => 
 // err ? console.log(err): console.log('Successfully created index.html!')       
 //  );
